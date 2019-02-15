7b14ae8d729b2d3919a39522d36f36882a1d3d61
Jordan Hansen <jhansen1@lenovo.com>
2019-02-14 19:34:33 -0700

    redeemable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { map } from 'rxjs/operators';
 
 @Component({
     selector: 'app-root',
@@ -8,10 +9,27 @@ import { AngularFirestore } from '@angular/fire/firestore';
 })
 export class AppComponent {
     public gifts;
-    public mainDescription;
+    documentToDomainObject = _ => {
+        const object = _.payload.doc.data();
+        object.id = _.payload.doc.id;
+        return object;
+    }
 
-    constructor(db: AngularFirestore) {
-        this.gifts = db.collection('gifts').valueChanges();
+    constructor(public db: AngularFirestore) {
+        this.gifts = db.collection('gifts').snapshotChanges()
+            .pipe(map(gifts => gifts.map(this.documentToDomainObject)));
+    }
+
+    public redeem(gift: any) {
+        const giftDoc = this.db.doc(`/gifts/${gift.id}`);
+        gift.redeemDate = new Date();
+        giftDoc.update(gift);
+    }
+
+    public convertTimestamp(timeStamp: any) {
+        // Create a new JavaScript Date object based on the timestamp
+        // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+        return new Date(timeStamp.seconds * 1000);
     }
 
 }
